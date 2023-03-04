1b6aa6ef95303d9626c59b217b887101013a19b8
Ilya Balagurov <balagurov1207@gmail.ru>
2023-03-04 10:35:25 +0300

    Стандартный класс

---
+++ Сlass/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Сlass
+{
+    public class Class1
+    {
+    }
+}
