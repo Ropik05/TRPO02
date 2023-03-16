6cca5f6f3576b3eba305888040cc1d18e1a16296
Aleksei Dyakov <aidyakov05@gmail.com>
2023-03-16 22:07:33 +0300

    Создан класс аудитория

--- ConsoleApp1/ClassCreator.cs
+++ ConsoleApp1/ClassCreator.cs
@@ -35,5 +35,37 @@ namespace ConsoleApp1
             string сокращение = Console.ReadLine();
             return new Var3_Дисциплина(название,сокращение);
         }
+        public static Var15_Оборудование Оборудование()
+        {
+            return new Var15_Оборудование();
+        }
+        public static Var9_Сотрудники Сотрудники()
+        {
+            return new Var_9Сотрудники();
+        }
+        public static Var2_Аудитория Аудитория()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine($"Введите название аудитории: ");
+                    int название_Аудитории = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"Введите кол-во мест в  аудитории: ");
+                    int количество_Мест = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"Введите кол-во окон в аудитории: ");
+                    int количество_Окон = int.Parse(Console.ReadLine());
+                    return new Var2_Аудитория(название_Аудитории, Сотрудники(), количество_Мест, количество_Окон, Оборудование());
+                }
+                catch
+                {
+                    Console.WriteLine("Введенв ошибка");
+                }
+            }
+        }
+    }
+
+    internal class Var_9Сотрудники : Var9_Сотрудники
+    {
     }
 }
--- ConsoleApp1/UI.cs
+++ ConsoleApp1/UI.cs
@@ -25,10 +25,26 @@ namespace ConsoleApp1
         {
 
         }
+        public static void Print(Var15_Оборудование оборудование)
+        {
+
+        }
+        public static void Print(Var9_Сотрудники сотрудники)
+        {
+
+        }
         public static void Print(Var3_Дисциплина Дисциплина)
         {
             Console.WriteLine($"Var3_Дисциплина Название = {Дисциплина.Название}");
             Console.WriteLine($"Var3_Дисциплина Сокращение = {Дисциплина.Сокращение}");
+        }
+        public static void Print(Var2_Аудитория аудитория)
+        {
+            Console.WriteLine($"Аудитория.Название_Аудитории={аудитория.Название_Аудитории}");
+            Print(аудитория.Сотрудники);
+            Console.WriteLine($"Аудитория.Количество_Мест={аудитория.Количество_Мест}");
+            Console.WriteLine($"Аудитория.Количество_Окон={аудитория.Количество_Окон}");
+            Print(аудитория.Оборудование);
         }
     }
 }
