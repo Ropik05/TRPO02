e44b049870ba17461b77d85622f4c3c6681c6286
stepan.kashnikovvv <stepan.kashnikovvv@gmail.com>
2023-03-17 21:04:47 +0300

    Создал класс Студент

---
+++ ClassLibrary1/Var5_Студент.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Var5_Студент
+    {
+        public string Фамилия { get; }
+        public string Имя { get; }
+        public string Отчество { get; }
+        public string Дата_Рождения { get; }
+        public Var4_Группа Группа { get; }
+        public Var5_Студент(string фамилия, string имя, string отчество, Var4_Группа группа, string дата_Рождения)
+        {
+            Фамилия = фамилия;
+            Имя = имя;
+            Отчество = отчество;
+            Дата_Рождения = дата_Рождения;
+            Группа = группа;
+        }
+    }
+}
